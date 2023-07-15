a39b748548272be0abbac231c1bc5e675f16e345
Nisa Efendioglu <nefendioglu@innova.com.tr>
2023-07-15 13:17:59 +0300

    Edit home page

--- app/src/main/java/com/nisaefendioglu/widgets/MainActivity.kt
+++ app/src/main/java/com/nisaefendioglu/widgets/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import com.nisaefendioglu.widgets.ui.theme.WidgetsTheme
 
@@ -22,7 +23,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Greeting("Widget App")
                 }
             }
         }
@@ -33,7 +34,9 @@ class MainActivity : ComponentActivity() {
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
-        modifier = modifier
+        modifier = modifier,
+        textAlign = TextAlign.Center,
+        style = MaterialTheme.typography.headlineLarge
     )
 }
 
